980e56518655f784c9837fce54b17e5c009cb6a3
welber <welberluan@gmail.com>
2023-05-18 01:37:21 -0300

    implementação da pagtina de hardskils

---
+++ js/app3.js
@@ -0,0 +1,17 @@
+const gM = document.querySelector(".galeria-modal");
+const iGM = document.querySelector(".galeria-modal img");
+
+function fecharGaleria(){
+
+    gM.style.visibility = "hidden";
+    iGM.style.transform="scale(0)";
+
+}
+
+function abrirGaleria(src){
+
+gM.style.visibility="visible";
+iGM.style.transform="scale(1)";
+iGM.src=src;
+
+}
